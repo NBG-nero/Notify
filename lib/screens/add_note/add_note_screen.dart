7d9cecde0a9692cd1ba1e519cc53cb096614522a
Aphra <oreofeojomo@gmail.com>
2022-09-08 23:04:08 +0100

    feat: update

--- lib/screens/add_note/add_note_screen.dart
+++ lib/screens/add_note/add_note_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../models/models.dart';
+import '../../utilities/constants/constants.dart';
 
 class AddNotescreen extends StatefulWidget {
   final Note? note;
@@ -21,6 +22,58 @@ class _AddNotescreenState extends State<AddNotescreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold( 
+      backgroundColor: NColors.secondaryColor,
+      appBar: AppBar( 
+        title: Text('Add Note'),
+        centerTitle: true, 
+        elevation: 0, 
+        backgroundColor: Colors.transparent,
+      ),
+      body: ListView( 
+        children:[ 
+           TextFormField( 
+            
+            decoration: const InputDecoration( 
+              border :InputBorder.none, 
+              hintText: 'Enter Title Text', 
+              hintStyle: TextStyle(fontSize:20.sp,fontWeight:FontWeight.bold,color:Colors.white54), 
+              
+            ),
+            style: const TextStyle( 
+
+                fontSize:20 .sp, 
+                fontWeight: FontWeight.bold, 
+                color:Colors.white,
+              ), 
+             
+             
+
+           ),
+           SizedBox(height:10.h), 
+           TextFormField( 
+            decoration: const InputDecoration(
+              border: InputBorder.none, 
+              hintText:'Description'
+              hintStyle:TextStylee(fontSize:10.sp,fontWeight:FontWeight.bold,color:Colors.white54), 
+              
+            ) ,
+            style: const TextStyle( 
+              fontSize:18.sp, 
+              color:Colors.white,
+            ),
+            maxlines:1000
+
+            )
+        ]
+      ), 
+      floatingActionButton:FloatingActionButton( 
+        onpressed:() { 
+
+        },
+        backgroundColors:Colors.white54, 
+        child:Icon(Icons.add,size:30.h,color:Collors.blue.shade900),
+      )
+    );
   }
 }
--- lib/utilities/constants/colors.dart
+++ lib/utilities/constants/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class NColors {
   NColors._();
   static Color primaryColor = Colors.blue.shade900;
+  static Color secondaryColor = const Color.fromARGB(255, 119, 154, 179);
 }
