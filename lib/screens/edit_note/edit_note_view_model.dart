f4b2efe2f9c8fe4b568f2b7c932ed4e5be0610da
Aphra <oreofeojomo@gmail.com>
2022-09-17 20:27:55 +0100

    feat: minor changes

--- lib/screens/edit_note/edit_note_view_model.dart
+++ lib/screens/edit_note/edit_note_view_model.dart
@@ -1,24 +1,22 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:stacked/stacked.dart';
 
-
 import '../../models/models.dart';
 import '../../utilities/constants/constants.dart';
 
 class EditNoteViewModel extends BaseViewModel {
-  
-
   saveNote(Note? note) {
     var hiveBox = Hive.box<Note>(noteBox);
     // hiveBox.putAt(note!.id!, value)
     hiveBox.delete(note!.id!);
     hiveBox.put(note.id, note);
-
   }
-updateNote(String? id, String? title, String? desc) {
-  Note updateNote = Note(id:id, title:title, desc: desc);
-}
 
-  
+  updateNote(String? id, String? title, String? desc) {
+    Note updateNote = Note(id: id, title: title, desc: desc);
+    var hiveBox = Hive.box<Note>(noteBox);
+    hiveBox.put(updateNote.id, updateNote);
 
+    return updateNote.id;
+  }
 }
