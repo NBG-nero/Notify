65dedd78baa863ac0030dc8823fa311918e2c303
Aphra <oreofeojomo@gmail.com>
2022-10-25 17:26:01 +0100

    feat: gridnotescard in progress

---
+++ lib/widgets/grid_note_card.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class GridNotesCard extends StatefulWidget {
+  const GridNotesCard({super.key});
+
+  @override
+  State<GridNotesCard> createState() => _GridNotesCardState();
+}
+
+class _GridNotesCardState extends State<GridNotesCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
