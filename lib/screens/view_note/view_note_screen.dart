1f79986bde8debefb111073a3241c7a803d40929
Aphra <oreofeojomo@gmail.com>
2022-11-03 22:56:16 +0100

    feat: minor deletions

--- lib/screens/view_note/view_note_screen.dart
+++ lib/screens/view_note/view_note_screen.dart
@@ -156,76 +156,7 @@ class _ViewNotescreenState extends State<ViewNotescreen> {
                     ),
                   )),
             ),
-            // bottomSheet: ClipRRect(
-            //   borderRadius: const BorderRadius.only(
-            //       topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-            //   child: Material(
-            //     elevation: 40,
-            //     child: InkWell(
-            //       onTap: () {
-            //         showModalBottomSheet(
-            //             backgroundColor: model.selectedColor,
-            //             context: context,
-            //             shape: const RoundedRectangleBorder(
-            //               borderRadius:
-            //                   BorderRadius.vertical(top: Radius.circular(25)),
-            //             ),
-            //             builder: (BuildContext context) {
-            //               return ListView.builder(
-            //                 scrollDirection: Axis.horizontal,
-            //                 itemCount: model.colors.length,
-            //                 itemBuilder: ((context, index) {
-            //                   Color? color = model.colors[index];
-            //                   return Container(
-            //                     height: 60.h,
-            //                     color: model.selectedColor,
-            //                     child: InkWell(
-            //                         onTap: () {
-            //                           model.setSelectedColor(color);
-            //                           // model.saveNoteColor(widget.note!.id);
-            //                           Navigator.of(context).pop();
-            //                           // model.changeTappedColor(index);
-            //                         },
-            //                         child: Padding(
-            //                           padding: const EdgeInsets.only(
-            //                               left: 8.0, right: 8),
-            //                           child: Container(
-            //                             height: 30.h,
-            //                             width: 30.w,
-            //                             padding: const EdgeInsets.all(2),
-            //                             decoration: const BoxDecoration(
-            //                                 color: Colors.white,
-            //                                 shape: BoxShape.circle),
-            //                             child: CircleAvatar(
-            //                               foregroundColor: Colors.grey,
-            //                               backgroundColor: color,
-            //                               // child: model.checkOrNot(index),
-            //                             ),
-            //                           ),
-            //                         )),
-            //                   );
-            //                 }),
-            //               );
-            //             });
-            //       },
-            //       child: Container(
-            //           height: 20.h,
-            //           width: MediaQuery.of(context).size.width,
-            //           color: Colors.blue,
-            //           // widget.note!.noteColor,
-            //           child: Center(
-            //             child: ClipRRect(
-            //               borderRadius: BorderRadius.circular(10),
-            //               child: SizedBox(
-            //                 height: 6.h,
-            //                 width: 60.w,
-            //                 child: Container(color: Colors.black),
-            //               ),
-            //             ),
-            //           )),
-            //     ),
-            //   ),
-            // ),
+            
 
 
           );
