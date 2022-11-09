9df350b7dbade9354900be6f778fae7358adf2e4
Aphra <oreofeojomo@gmail.com>
2022-11-09 22:28:37 +0100

    feat: animationController ish

--- lib/screens/add_note/add_note_view_model.dart
+++ lib/screens/add_note/add_note_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
-
 import 'package:uuid/uuid.dart';
 
 import '../../models/models.dart';
@@ -10,10 +9,10 @@ import '../base_model.dart';
 class AddNoteViewModel extends BaseModel {
   var uuid = const Uuid();
   bool? complete = false;
-
-AddNoteViewModel(){ 
-  loadNotecolorFromprefs();
-}
+ 
+  AddNoteViewModel() {
+    loadNotecolorFromprefs();
+  }
 
   setComplete(value) {
     note?.complete = complete;
@@ -23,15 +22,14 @@ AddNoteViewModel(){
 
   String? addNote({required String title, required String desc}) {
     Note note = Note(
-      title: title,
-      desc: desc,
-      complete: false,
-      id: uuid.v4(),
-      isEdited: false,
-      dateCreated: DateTime.now(),
-      updatedDate: DateTime.now(), 
-      noteColor: selectedColor
-    );
+        title: title,
+        desc: desc,
+        complete: false,
+        id: uuid.v4(),
+        isEdited: false,
+        dateCreated: DateTime.now(),
+        updatedDate: DateTime.now(),
+        noteColor: selectedColor);
     var hiveBox = Hive.box<Note>(noteBox);
     hiveBox.put(note.id, note);
 
